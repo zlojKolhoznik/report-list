b956193dfac534f8d9908610353cae28157f0548
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-04 18:08:37 +0200

    Wrote documentation for AccountManager.cs

--- ServerApp/AccountManager.cs
+++ ServerApp/AccountManager.cs
@@ -1,13 +1,10 @@
 ﻿using DatabaseClasses;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServerApp
 {
+    /// <summary>
+    /// Class that does account-related work
+    /// </summary>
     internal class AccountManager
     {
         private ReporlistContext context;
