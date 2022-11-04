5c99c4f88261f5f015cd69f2f7e8ed3f81e710e4
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-04 21:03:17 +0200

    Added DatabaseAccessManager.cs

---
+++ ServerApp/DatabaseAccessManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    internal abstract class DatabaseAccessManager
+    {
+        protected readonly string connStr;
+
+        protected DatabaseAccessManager(string connStr)
+        {
+            this.connStr = connStr;
+        }
+    }
+}
