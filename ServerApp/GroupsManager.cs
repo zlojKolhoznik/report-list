5502b2f9c8d8b9df9869007de610313990e0656d
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-04 21:29:48 +0200

    Added GroupsManager.cs

---
+++ ServerApp/GroupsManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    // TODO: Implement GroupsManager
+    internal class GroupsManager : DatabaseAccessManager
+    {
+        public GroupsManager(string connStr) : base(connStr)
+        {
+
+        }
+    }
+}
