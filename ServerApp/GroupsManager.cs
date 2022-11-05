aff0c6d9632634d0e1f55fa0881995fa26a475e2
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-05 15:04:09 +0200

    Implemented GroupsManager

--- ServerApp/GroupsManager.cs
+++ ServerApp/GroupsManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,76 @@ using System.Threading.Tasks;
 
 namespace ServerApp
 {
-    // TODO: Implement GroupsManager
     internal class GroupsManager : DatabaseAccessManager
     {
+        private static object locker = new object();
+
         public GroupsManager(string connStr) : base(connStr)
         {
 
+        }
+
+        public List<Group> GetGroups(Subject subject)
+        {
+            var result = new List<Group>();
+            foreach (var lesson in subject.Lessons)
+            {
+                if(!result.Any(g => g.Id == lesson.Group.Id))
+                {
+                    result.Add(lesson.Group);
+                }
+            }
+            return result;
+        }
+
+        public List<Group> GetGroups(Teacher teacher)
+        {
+            var result = new List<Group>();
+            foreach (var lesson in teacher.Lessons)
+            {
+                if(!result.Any(g => g.Id == lesson.Group.Id))
+                {
+                    result.Add(lesson.Group);
+                }
+            }
+            return result;
+        }
+
+        public void AddGroup(Group group)
+        {
+            lock (locker)
+            {
+                using (var context = new ReporlistContext(connStr))
+                {
+                    if (context.Groups.Any(g => g.Name == group.Name))
+                    {
+                        throw new InvalidOperationException("Group with this name already exists in database");
+                    }
+                    context.Groups.Add(group);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void RenameGroup(Group group, string newName)
+        {
+            if (group.Name == newName)
+            {
+                throw new InvalidOperationException("Cannot change the name of the group to the current name");
+            }
+            lock (locker)
+            {
+                using (var context = new ReporlistContext(connStr))
+                {
+                    Group? groupFromDb = context.Groups.FirstOrDefault(g => g.Name == group.Name);
+                    if (groupFromDb == null)
+                    {
+                        throw new InvalidOperationException("Group you are trying to access is not in the database");
+                    }
+                    groupFromDb.Name = newName;
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
