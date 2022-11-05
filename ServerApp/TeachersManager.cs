eb06612fe45b397979cfb8ea299e2ef9ac81a697
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-05 17:29:45 +0200

    Added TeachersManager.cs

---
+++ ServerApp/TeachersManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    internal class TeachersManager : DatabaseAccessManager
+    {
+        private object locker = new object();
+
+        public TeachersManager(string connStr) : base(connStr)
+        {
+
+        }
+    }
+}
