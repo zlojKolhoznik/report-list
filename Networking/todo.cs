7e04cb0b85b72911e955b6bf9545ebecbb7e3701
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-07 22:15:41 +0200

    Changes in TODO file

--- Networking/todo.cs
+++ Networking/todo.cs
@@ -1,3 +1,4 @@
-﻿// TODO: Implement all View Models
-// TODO: Implement request processing
+﻿// TODO: Implement request processing
 // TODO: Add GetStudents(Group) request Type
+// TODO: Add GetMarks(Student) request Type
+// TODO: Refactor request processers: managers -> extension methods
