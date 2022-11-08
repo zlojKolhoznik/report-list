c004a23311496cfb37731d431fad0a352bb0860a
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-08 14:15:56 +0200

    Changed TODO file

--- Networking/todo.cs
+++ Networking/todo.cs
@@ -1,4 +1,1 @@
 ﻿// TODO: Implement request processing
-// TODO: Add GetStudents(Group) request Type
-// TODO: Add GetMarks(Student) request Type
-// TODO: Refactor request processers: managers -> extension methods
