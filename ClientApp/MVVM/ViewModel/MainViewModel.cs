c70a65c02cfa86930ab61b3573fed0a405700c93
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-12 11:59:00 +0200

    Added MainViewModel.cs

---
+++ ClientApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace ClientApp.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public MainViewModel()
+        {
+            //App app = (App)Application.Current;
+            //if (app.User!.IsAdmin)
+            //{
+            //    CurrentViewModel = new AdminViewModel();
+            //}
+        }
+        public object CurrentViewModel { get; set; }
+
+        public string Role { get; set; } = "Student";
+        public string Username { get; set; } = "stud3203";
+        public string FullName { get; set; } = "Olexander Avramenko";
+    }
+}
