a4d6076eb9676cb771c24fd0144af052a209a86c
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-13 20:53:25 +0200

    Added TeachersMarksModel.cs

---
+++ ClientApp/MVVM/Model/TeachersMarksModel.cs
@@ -0,0 +1,84 @@
+﻿using Networking.DataViews;
+using Networking.Requests;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClientApp.MVVM.Model
+{
+    internal class TeachersMarksModel
+    {
+        private TeacherDataView teacher;
+        private App app;
+
+        public TeachersMarksModel()
+        {
+            app = (App)Application.Current;
+            teacher = GetTeacher(app.User!.Id);
+        }
+
+        public TeacherDataView Teacher => teacher;
+
+        private TeacherDataView GetTeacher(int userId)
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetTeacher, UserId = userId};
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            return response.Teacher!;
+        }
+
+        public List<GroupDataView> GetGroups()
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetGroups, TeacherId = teacher.Id };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            return response.Groups!;
+        }
+
+        public List<SubjectDataView> GetSubjects(GroupDataView group)
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetSubjects, GroupId = group.Id };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            return response.Subjects!;
+        }
+
+        public List<string> GetMarksViews(int groupId, int? subjectId)
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetMarks, TeacherId = teacher.Id, GroupId = groupId, SubjectId = subjectId };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            var result = new List<string>();
+            foreach (var mark in response.Marks!)
+            {
+                if (mark.Lesson != null)
+                {
+                    result.Add($"Lesson {mark.Lesson.Subject} {new DateTime(mark.Lesson.Date).ToShortDateString()} {mark.Student!.Name} {mark.Student!.Surname} - {mark.Value}");
+                    continue;
+                }
+                if (mark.Homework != null)
+                {
+                    result.Add($"Homework {mark.Homework.Subject} {new DateTime(mark.Homework.DueDate).ToShortDateString()} {mark.Student!.Name} {mark.Student!.Surname} - {mark.Value}");
+                    continue;
+                }
+            }
+            return result;
+        }
+    }
+}
--- ClientApp/MVVM/ViewModel/TeachersMarksViewModel.cs
+++ ClientApp/MVVM/ViewModel/TeachersMarksViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ClientApp.Core;
+using ClientApp.MVVM.Model;
 using Networking.DataViews;
 using Networking.Requests;
 using Newtonsoft.Json;
@@ -15,44 +16,26 @@ namespace ClientApp.MVVM.ViewModel
 		private List<GroupDataView> groups;
 		private List<SubjectDataView> subjects;
 		private List<string> marksViews;
-		private App app;
-		private TeacherDataView teacher;
 		private RelayCommand? getMarks;
+		private TeachersMarksModel model;
 		private int? selectedSubjectId;
 		private int selectedGroupId;
 
 		public TeachersMarksViewModel()
 		{
-			MarksViews = new List<string>();
-			app = (App)Application.Current;
-			RequestOptions request = new RequestOptions() { RequestType = RequestType.GetTeacher, UserId = app.User!.Id };
-			string json = JsonConvert.SerializeObject(request);
-			byte[] bytes = Encoding.UTF8.GetBytes(json);
-			bytes = app.SendRequestAndReceiveResponse(bytes);
-			json = Encoding.UTF8.GetString(bytes);
-			ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
-			teacher = response.Teacher!;
-			request = new RequestOptions() { RequestType = RequestType.GetGroups, TeacherId = teacher.Id };
-			json = JsonConvert.SerializeObject(request);
-			bytes = Encoding.UTF8.GetBytes(json);
-			bytes = app.SendRequestAndReceiveResponse(bytes);
-			json = Encoding.UTF8.GetString(bytes);
-			response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
-			groups = response.Groups!;
-			subjects = new List<SubjectDataView>();
-			foreach (var group in Groups)
-			{
-                request = new RequestOptions() { RequestType = RequestType.GetSubjects, GroupId = group.Id };
-                json = JsonConvert.SerializeObject(request);
-                bytes = Encoding.UTF8.GetBytes(json);
-                bytes = app.SendRequestAndReceiveResponse(bytes);
-                json = Encoding.UTF8.GetString(bytes);
-                response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
-                subjects.AddRange(response.Subjects!.Where(s=>!subjects.Select(s1=>s1.Id).Contains(s.Id) && teacher.SubjectsIds.Contains((int)s.Id)));
+            MarksViews = new List<string>();
+            model = new TeachersMarksModel();
+            Groups = model.GetGroups();
+            subjects = new List<SubjectDataView>();
+            foreach (var group in Groups)
+            {
+                List<SubjectDataView> temp = model.GetSubjects(group);
+                subjects.AddRange(temp.ExceptBy(subjects.Select(s => s.Id), s => s.Id));
             }
-			subjects.Insert(0, new SubjectDataView { Name = "Any", Id = null });
-			selectedSubjectId = null;
-			selectedGroupId = (int)groups.First().Id;
+			subjects = subjects.Where(s => model.Teacher.SubjectsIds.Contains( (int)s.Id!)).ToList();
+            subjects.Insert(0, new SubjectDataView { Name = "Any", Id = null });
+            selectedSubjectId = null;
+            selectedGroupId = (int)groups!.First().Id!;
         }
 
 		public List<string> MarksViews
@@ -106,6 +89,7 @@ namespace ClientApp.MVVM.ViewModel
 				}
 			}
 		}
+
 		public int SelectedGroupId
 		{
 			get => selectedGroupId;
@@ -123,27 +107,7 @@ namespace ClientApp.MVVM.ViewModel
 		{
 			get => getMarks ??= new RelayCommand((obj) =>
 			{
-                RequestOptions request = new RequestOptions() { RequestType = RequestType.GetMarks, TeacherId = teacher.Id, GroupId = SelectedGroupId, SubjectId = SelectedSubjectId};
-                string json = JsonConvert.SerializeObject(request);
-                byte[] bytes = Encoding.UTF8.GetBytes(json);
-                bytes = app.SendRequestAndReceiveResponse(bytes);
-                json = Encoding.UTF8.GetString(bytes);
-                ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
-				var list = new List<string>();
-				foreach (var mark in response.Marks!)
-				{
-					if (mark.Lesson != null)
-					{
-						list.Add($"Lesson {mark.Lesson.Subject} {new DateTime(mark.Lesson.Date).ToShortDateString()} {mark.Student!.Name} {mark.Student!.Surname} - {mark.Value}");
-						continue;
-					}
-					if (mark.Homework != null)
-					{
-                        list.Add($"Homework {mark.Homework.Subject} {new DateTime(mark.Homework.DueDate).ToShortDateString()} {mark.Student!.Name} {mark.Student!.Surname} - {mark.Value}");
-                        continue;
-                    }
-				}
-				MarksViews = list;
+				MarksViews = model.GetMarksViews(SelectedGroupId, SelectedSubjectId);
             });
 			set
 			{
