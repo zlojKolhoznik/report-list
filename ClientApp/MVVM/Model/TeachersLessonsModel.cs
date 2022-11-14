c163b014a129017d576301faa5cbd0a125a5b12f
zlojKolhoznik <rportyanko10@gmail.com>
2022-11-14 11:06:40 +0200

    Created Teachers lessons page

---
+++ ClientApp/MVVM/Model/TeachersLessonsModel.cs
@@ -0,0 +1,190 @@
+﻿using Networking.DataViews;
+using Networking.Requests;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClientApp.MVVM.Model
+{
+    class TeachersLessonsModel
+    {
+        App app;
+        TeacherDataView teacher;
+
+        public TeachersLessonsModel()
+        {
+            app = (App)Application.Current;
+            teacher = GetTeacher(app.User!.Id);
+        }
+
+        public TeacherDataView Teacher => teacher;
+
+        private TeacherDataView GetTeacher(int userId)
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetTeacher, UserId = userId };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            if (!response.Success)
+            {
+                throw new Exception(response.ErrorMessage);
+            }
+            return response.Teacher!;
+        }
+
+        public List<GroupDataView> GetGroups()
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetGroups, TeacherId = teacher.Id };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            return response.Groups!;
+        }
+
+        public List<SubjectDataView> GetSubjects(GroupDataView group)
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetSubjects, GroupId = group.Id };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            return response.Subjects!.Where(s=>teacher.SubjectsIds.Contains((int)s.Id!)).ToList();
+        }
+
+        public List<string> GetLesons(int? subjectId = null, int? groupId = null, DateTime? date = null)
+        {
+            if (subjectId == null && groupId == null && date == null)
+            {
+                throw new ArgumentNullException($"{nameof(subjectId)} {nameof(groupId)} {nameof(date)}", "To get the lessons list should be provided at least the subject, the group or the date");
+            }
+            if (subjectId != null && groupId != null && date != null)
+            {
+                List<LessonDataView> lessonsSubject = GetLessonsBySubject((int)subjectId);
+                List<LessonDataView> lessonsGroup = GetLessonsByGroup((int)groupId);
+                List<LessonDataView> lessonsDate = GetLessonsByDate((DateTime)date);
+                List<LessonDataView> temp = lessonsSubject.IntersectBy(lessonsGroup.Select(l => l.Id), l => l.Id).ToList();
+                List<string> result = new List<string>();
+                foreach (var lesson in temp.IntersectBy(lessonsDate.Select(l => l.Id), l => l.Id))
+                {
+                    result.Add($"{new DateTime(lesson.Date).ToString("dd.MM HH:mm")}. {lesson.Topic}");
+                }
+                return result;
+            }
+            if (subjectId != null && groupId != null)
+            {
+                List<LessonDataView> lessonsSubject = GetLessonsBySubject((int)subjectId);
+                List<LessonDataView> lessonsGroup = GetLessonsByGroup((int)groupId);
+                List<string> result = new List<string>();
+                foreach (var lesson in lessonsSubject.IntersectBy(lessonsGroup.Select(l => l.Id), l => l.Id))
+                {
+                    result.Add($"{new DateTime(lesson.Date).ToString("dd.MM HH:mm")}. {lesson.Topic}");
+                }
+                return result;
+            }
+            if (subjectId != null && date != null)
+            {
+                List<LessonDataView> lessonsSubject = GetLessonsBySubject((int)subjectId);
+                List<LessonDataView> lessonsDate = GetLessonsByDate((DateTime)date);
+                List<string> result = new List<string>();
+                foreach (var lesson in lessonsSubject.IntersectBy(lessonsDate.Select(l => l.Id), l => l.Id))
+                {
+                    result.Add($"{new DateTime(lesson.Date).ToString("dd.MM HH:mm")}. {lesson.Topic}");
+                }
+                return result;
+            }
+            if (groupId != null && date != null)
+            {
+                List<LessonDataView> lessonsGroup = GetLessonsByGroup((int)groupId);
+                List<LessonDataView> lessonsDate = GetLessonsByDate((DateTime)date);
+                List<string> result = new List<string>();
+                foreach (var lesson in lessonsDate.IntersectBy(lessonsGroup.Select(l => l.Id), l => l.Id))
+                {
+                    result.Add($"{new DateTime(lesson.Date).ToString("dd.MM HH:mm")}. {lesson.Topic}");
+                }
+                return result;
+            }
+            if (subjectId != null)
+            {
+                List<string> result = new List<string>();
+                foreach (var lesson in GetLessonsBySubject((int)subjectId))
+                {
+                    result.Add($"{new DateTime(lesson.Date).ToString("dd.MM HH:mm")}. {lesson.Topic}");
+                }
+                return result;
+            }
+            if (groupId != null)
+            {
+                List<string> result = new List<string>();
+                foreach (var lesson in GetLessonsByGroup((int)groupId))
+                {
+                    result.Add($"{new DateTime(lesson.Date).ToString("dd.MM HH:mm")}. {lesson.Topic}");
+                }
+                return result;
+            }
+            if (date != null)
+            {
+                List<string> result = new List<string>();
+                foreach (var lesson in GetLessonsByDate((DateTime)date))
+                {
+                    result.Add($"{new DateTime(lesson.Date).ToString("dd.MM HH:mm")}. {lesson.Topic}");
+                }
+                return result;
+            }
+            throw new Exception();
+        }
+
+        private List<LessonDataView> GetLessonsBySubject(int subjectId)
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetLessons, SubjectId = subjectId, TeacherId = teacher.Id };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            if (!response.Success)
+            {
+                throw new Exception(response.ErrorMessage);
+            }
+            return response.Lessons!;
+        }
+
+        private List<LessonDataView> GetLessonsByDate(DateTime date)
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetLessons, LessonDate = date.Ticks, TeacherId = teacher.Id };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            if (!response.Success)
+            {
+                throw new Exception(response.ErrorMessage);
+            }
+            return response.Lessons!;
+        }
+
+        private List<LessonDataView> GetLessonsByGroup(int groupId)
+        {
+            RequestOptions request = new RequestOptions() { RequestType = RequestType.GetLessons, GroupId = groupId, TeacherId = teacher.Id };
+            string json = JsonConvert.SerializeObject(request);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            bytes = app.SendRequestAndReceiveResponse(bytes);
+            json = Encoding.UTF8.GetString(bytes);
+            ResponseOptions response = JsonConvert.DeserializeObject<ResponseOptions>(json)!;
+            if (!response.Success)
+            {
+                throw new Exception(response.ErrorMessage);
+            }
+            return response.Lessons!;
+        }
+    }
+}
--- ClientApp/MVVM/ViewModel/TeachersLessonsViewModel.cs
+++ ClientApp/MVVM/ViewModel/TeachersLessonsViewModel.cs
@@ -1,12 +1,157 @@
-﻿using System;
+﻿using ClientApp.Core;
+using ClientApp.MVVM.Model;
+using Networking.DataViews;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace ClientApp.MVVM.ViewModel
 {
     internal class TeachersLessonsViewModel : LessonsViewModel
     {
+        private TeachersLessonsModel model;
+        private List<SubjectDataView> subjects;
+        private List<GroupDataView> groups;
+        private List<string> lessonsView;
+        private bool isDateIncluded;
+        private DateTime date;
+        private int? selectedGroupId;
+        private int? selectedSubjectId;
+        private RelayCommand? getLessons;
+
+        public TeachersLessonsViewModel()
+        {
+            model = new TeachersLessonsModel();
+            Groups = model.GetGroups();
+            Subjects = new List<SubjectDataView>();
+            foreach (var group in Groups)
+            {
+                List<SubjectDataView> temp = model.GetSubjects(group);
+                Subjects.AddRange(temp.ExceptBy(Subjects.Select(s => s.Id), s => s.Id));
+            }
+            Groups.Insert(0, new GroupDataView() { Id = null, Name = "Будь-яка" });
+            Subjects.Insert(0, new SubjectDataView() { Id = null, Name = "Будь-який" });
+            IsDateIncluded = true;
+            Date = DateTime.Now.Date;
+        }
+
+        public List<SubjectDataView> Subjects
+        {
+            get => subjects;
+            set
+            {
+                if (subjects != value)
+                {
+                    subjects = value;
+                    OnPropertyChanged(nameof(Subjects));
+                }
+            }
+        }
+
+        public List<GroupDataView> Groups
+        {
+            get => groups;
+            set
+            {
+                if (groups != value)
+                {
+                    groups = value;
+                    OnPropertyChanged(nameof(Groups));
+                }
+            }
+        }
+
+        public List<string> LessonsView
+        {
+            get => lessonsView;
+            set
+            {
+                if (lessonsView != value)
+                {
+                    lessonsView = value;
+                    OnPropertyChanged(nameof(LessonsView));
+                }
+            }
+        }
+
+        public bool IsDateIncluded
+        {
+            get => isDateIncluded;
+            set
+            {
+                if (isDateIncluded != value)
+                {
+                    isDateIncluded = value;
+                    OnPropertyChanged(nameof(IsDateIncluded));
+                }
+            }
+        }
+
+        public DateTime Date
+        {
+            get => date; 
+            set
+            {
+                if (date != value)
+                {
+                    date = value;
+                    OnPropertyChanged(nameof(Date));
+                }
+            }
+        }
+
+        public int? SelectedGroupId
+        {
+            get => selectedGroupId;
+            set
+            {
+                if (selectedGroupId != value)
+                {
+                    selectedGroupId = value;
+                    OnPropertyChanged(nameof(SelectedGroupId));
+                }
+            }
+        }
+
+        public int? SelectedSubjectId
+        {
+            get => selectedSubjectId;
+            set
+            {
+                if (selectedSubjectId != value)
+                {
+                    selectedSubjectId = value;
+                    OnPropertyChanged(nameof(SelectedSubjectId));
+                }
+            }
+        }
+
+        public RelayCommand GetLessons
+        {
+            get => getLessons ??= new RelayCommand((obj) =>
+            {
+                try
+                {
+                    LessonsView = model.GetLesons(SelectedSubjectId, SelectedGroupId, IsDateIncluded ? Date : null);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Unexpected error happened", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            });
+            set
+            {
+                if (getLessons != value)
+                {
+                    getLessons = value;
+                    OnPropertyChanged(nameof(GetLessons));
+                }
+            }
+        }
     }
 }
